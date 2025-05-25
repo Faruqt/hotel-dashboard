186df654eeb41a5e063bdd5ca7fb7f2d37b885d6
Faruqt <faruqabdulsalam@yahoo.com>
2025-05-25 21:18:00 +0100

    feat: create common helper method to handle html to pdf generation

--- backend/app/utils/common.py
+++ backend/app/utils/common.py
@@ -1,10 +1,31 @@
 # library imports
 import logging
 import math
+import re
+import os
 from datetime import datetime
+from weasyprint import HTML
+from typing import Optional
+
+# local imports
+from app.config.settings import PDF_DIR_PATH, STATIC_DIR_PATH
 
 # create a logger instance
 logger = logging.getLogger(__name__)
+
+
+def sanitize_filename(title: str) -> str:
+    """
+    Sanitize a filename by replacing spaces with underscores and removing non-alphanumeric characters.
+
+    Args:
+        title (str): The title to sanitize.
+
+    Returns:
+        str: The sanitized filename.
+    """
+    # Replace spaces with underscores and remove non-alphanumeric characters
+    return re.sub(r"[^a-zA-Z0-9_-]", "", title.replace(" ", "_")).lower()
 
 
 def convert_string_to_datetime(date_string: str) -> datetime:
@@ -65,3 +86,49 @@ def calculate_pagination_metadata(
         "page_size": page_size,
         "total_items": total_items,
     }
+
+
+def create_pdf_from_html(
+    pdf_name: str,
+    html_content: str,
+    caller: str,
+) -> str:
+    """
+    Create a PDF using a rendered HTML template.
+
+    Args:
+        pdf_name (str): The name for the PDF file to be created.
+        html_content (str): The HTML content to render in the PDF.
+        caller (str): The caller of the function, used for logging.
+
+    Returns:
+        sanitized_name (str): The name of the created PDF file.
+
+    Raises:
+        HTTPException: If an error occurs while creating the PDF.
+    """
+    try:
+        # Ensure the PDF directory exists
+        os.makedirs(PDF_DIR_PATH, exist_ok=True)
+
+        sanitized_name = sanitize_filename(pdf_name) + ".pdf"
+        logger.info(
+            f"PDF with name: {pdf_name} created successfully created for {caller}"
+        )
+
+        pdf_path = os.path.join(PDF_DIR_PATH, sanitized_name)
+
+        print("Creating PDF at path:", STATIC_DIR_PATH)
+
+        HTML(string=html_content, base_url=STATIC_DIR_PATH).write_pdf(
+            pdf_path,
+        )
+
+        return sanitized_name
+
+    except Exception as e:
+        logger.error(
+            f"An error occurred while creating PD from html content for {caller}: {e}",
+            exc_info=True,
+        )
+        raise e
