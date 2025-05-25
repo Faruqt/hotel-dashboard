de525011dd09827a1145c53bb4137b94beaa8a54
Faruqt <faruqabdulsalam@yahoo.com>
2025-05-25 21:17:14 +0100

    chore: update paths in settings config

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -2,8 +2,13 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sqlite.db")
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_DIR_PATH = os.path.join(BASE_DIR, "static")
+TEMPLATES_DIR_PATH = os.path.join(BASE_DIR, "templates")
 IMAGE_DIR = "images"
+IMAGE_DIR_PATH = os.path.join(STATIC_DIR_PATH, IMAGE_DIR)
+PDF_DIR = "pdfs"
+PDF_DIR_PATH = os.path.join(STATIC_DIR_PATH, PDF_DIR)
 DATE_OUTPUT_FORMAT = "%d/%m/%Y"
 MAX_PAGINATION_LIMIT = 100
 DEFAULT_PAGINATION_SIZE = 20
+APP_URL = os.getenv("APP_URL", "http://127.0.1:8000")
