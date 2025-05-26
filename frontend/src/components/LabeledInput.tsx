96c2301cf2d1c5a4a9830090fc4fd31b0b2dde74
Faruqt <faruqabdulsalam@yahoo.com>
2025-05-26 15:24:13 +0100

    feat: create labeled input component

---
+++ frontend/src/components/LabeledInput.tsx
@@ -0,0 +1,18 @@
+interface LabeledInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  className?: string;
+}
+
+function LabeledInput({ label, className ="", ...inputProps }: LabeledInputProps) {
+  return (
+    <div className={`flex flex-col mb-[20px] ${className ?? ''}`}>
+    <span className="font-semibold text-xs mb-[7px]">{label}</span>
+    <input
+      className="bg-input w-[600px] h-[44px] p-[15px] text-dark"
+      {...inputProps}
+    />
+  </div>
+  );
+}
+
+export default LabeledInput;
