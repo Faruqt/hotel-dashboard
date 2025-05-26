cc55cc611bf560196525ec493a6b574678afd70f
Faruqt <faruqabdulsalam@yahoo.com>
2025-05-26 15:23:39 +0100

    chore: register react toastify css in default css file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,20 @@
 // library imports
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 // local imports
-import App from './App';
+import App from "./App";
 
 // styles imports
-import './index.css';
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
